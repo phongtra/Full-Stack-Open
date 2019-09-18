5cf654b74ad301e861aabba4cadd5ee2e5997ca3
Phongtran1998 <hoang_phong98@me.com>
2019-09-18 15:28:58 +0300

    completed 1.4

--- part1/part1-exercise/src/components/Total.js
+++ part1/part1-exercise/src/components/Total.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Total = ({ total }) => {
-  return <p>Number of exercises {total}</p>;
+  return (
+    <p>
+      Number of exercises{' '}
+      {total[0].exercises + total[1].exercises + total[2].exercises}
+    </p>
+  );
 };
 
 export default Total;
