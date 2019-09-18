1d6ca88683ec625f75e831acd5368dcfe05cbd44
Phongtran1998 <hoang_phong98@me.com>
2019-09-18 15:25:33 +0300

    completed 1.3

--- part1/part1-exercise/src/components/App.js
+++ part1/part1-exercise/src/components/App.js
@@ -5,19 +5,24 @@ import Total from './Total';
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of component';
-  const exercises3 = 14;
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  };
   const parts = [part1, part2, part3];
-  const exercises = [exercises1, exercises2, exercises3];
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} exercises={exercises} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
     </>
   );
 };
