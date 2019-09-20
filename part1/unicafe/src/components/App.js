d71ad01412f9654e29e3586acff09e4f7132994d
Phongtran1998 <hoang_phong98@me.com>
2019-09-20 22:26:36 +0300

    finish 1.7

--- part1/unicafe/src/components/App.js
+++ part1/unicafe/src/components/App.js
@@ -4,6 +4,16 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const renderAverage = () => {
+    let positiveSum = good - bad;
+    return positiveSum / (good + neutral + bad) || 0;
+  };
+
+  const renderPercentage = () => {
+    return (good / (good + bad + neutral)) * 100 || 0;
+  };
+
   return (
     <>
       <h1>give feedback</h1>
@@ -14,6 +24,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + bad + neutral}</p>
+      <p>average {renderAverage()}</p>
+      <p>percentage {renderPercentage()}</p>
     </>
   );
 };
