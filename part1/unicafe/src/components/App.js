d47b4cc55873cf6f3dc53e9393002e85576f5b9e
Phongtran1998 <hoang_phong98@me.com>
2019-09-21 19:18:55 +0300

    finish 1.10

--- part1/unicafe/src/components/App.js
+++ part1/unicafe/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Statistics from './Statistics';
+import Button from './Button';
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -15,20 +16,28 @@ const App = () => {
     return (good / (good + bad + neutral)) * 100 || 0;
   };
 
+  const renderFeedBack = () => {
+    if (good === 0 && neutral === 0 && bad === 0) {
+      return <p>No feedbacks given</p>;
+    }
+    return (
+      <div>
+        <Statistics text={'good'} value={good} />
+        <Statistics text={'neutral'} value={neutral} />
+        <Statistics text={'bad'} value={bad} />
+        <Statistics text={'average'} value={renderAverage()} />
+        <Statistics text={'percentage'} value={renderPercentage()} />
+      </div>
+    );
+  };
   return (
     <>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>netral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text={'good'} />
+      <Button onClick={() => setNeutral(neutral + 1)} text={'neutral'} />
+      <Button onClick={() => setBad(bad + 1)} text={'bad'} />
       <h1>statistics</h1>
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        renderAverage={renderAverage}
-        renderPercentage={renderPercentage}
-      />
+      {renderFeedBack()}
     </>
   );
 };
--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,23 +1,11 @@
 import React from 'react';
 
-const Statistics = ({
-  good,
-  neutral,
-  bad,
-  renderAverage,
-  renderPercentage
-}) => {
-  if (good == 0 && neutral == 0 && bad == 0) {
-    return <p>No feedbacks given</p>;
-  }
+const Statistics = ({ text, value }) => {
   return (
     <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {renderAverage()}</p>
-      <p>percentage {renderPercentage()}</p>
+      <p>
+        {text} {value}{' '}
+      </p>
     </div>
   );
 };
