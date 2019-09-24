f0ea618136804aba25f17b94868f71ace0063e76
Phongtran1998 <hoang_phong98@me.com>
2019-09-24 06:47:25 +0300

    finish 1.13

--- part1/anecdotes/src/components/App.js
+++ part1/anecdotes/src/components/App.js
@@ -2,13 +2,25 @@ import React, { useState } from 'react';
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
-
+  const [point, setPoint] = useState(
+    Array.apply(null, new Array(6)).map(Number.prototype.valueOf, 0)
+  );
   return (
     <div>
       <p>{anecdotes[selected]}</p>
       <button onClick={() => setSelected(Math.floor(Math.random() * 6))}>
         next anecdote
       </button>
+      <button
+        onClick={() => {
+          const copy = [...point];
+          copy[selected]++;
+          setPoint(copy);
+        }}
+      >
+        vote
+      </button>
+      <p>has {point[selected]} votes</p>
     </div>
   );
 };
