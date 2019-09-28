62d3717638dad7d12d84f41bf99d25d8b9682fb7
Phongtran1998 <hoang_phong98@me.com>
2019-09-28 10:14:42 +0300

    finish 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
-
+  const [filter, setFilter] = useState('');
+  const renderFilter = () => {
+    if (!filter) {
+      return persons;
+    }
+    return persons.filter(person => person.name.indexOf(filter) > -1);
+  };
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with{' '}
+      <input value={filter} onChange={e => setFilter(e.target.value)} />
+      <h3>add a new</h3>
       <form
         onSubmit={e => {
           e.preventDefault();
@@ -37,11 +51,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
-        ))}
+        {renderFilter().map(result => {
+          return (
+            <li key={result.name}>
+              {result.name} {result.number}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
